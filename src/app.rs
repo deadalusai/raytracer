4dd54cab9db2960197408a4811cd3268f605beb1
Benjamin Fox <deadalus.ai@gmail.com>
2022-11-27 10:15:10 +1300

    Update to egui 0.19

--- src/app.rs
+++ src/app.rs
@@ -284,7 +284,7 @@ impl eframe::App for App {
             if let Some(job) = self.render_job.as_ref() {
                 let (tex_dim, tex_data) = job.buffer.get_raw_rgba_data();
                 let tex_data = egui::ColorImage::from_rgba_unmultiplied(tex_dim, tex_data);
-                let tex_id = ctx.load_texture("output_tex", tex_data);
+                let tex_id = ctx.load_texture("output_tex", tex_data, egui::TextureFilter::Linear);
                 self.output_texture = Some((tex_id, tex_dim));
             }
         }
